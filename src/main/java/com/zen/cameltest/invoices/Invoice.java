2e7ec091f89200bcccc4cde69a56008568bffb79
Luca Zenobi <luzenobi@gmail.com>
2019-12-02 23:26:51 +0100

    First commit

---
+++ src/main/java/com/zen/cameltest/invoices/Invoice.java
@@ -0,0 +1,53 @@
+package com.zen.cameltest.invoices;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Invoice {
+
+  private Long id;
+  private String code;
+  private LocalDateTime creationDate;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Invoice invoice = (Invoice) o;
+    return Objects.equals(id, invoice.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public LocalDateTime getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(LocalDateTime creationDate) {
+    this.creationDate = creationDate;
+  }
+
+}
